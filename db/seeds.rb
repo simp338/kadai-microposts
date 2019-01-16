dfd24350795752684bd9f13d07d26bdf313db6be
simp338 <45528081+simp338@users.noreply.github.com>
2019-01-16 22:26:35 +0000

    follow, unfollow

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..50).each do |number|
+  user = User.new(name: "test#{number.to_s}", email: "#{number.to_s}@#{number.to_s}.com", password: number.to_s)
+  user.save
+end
