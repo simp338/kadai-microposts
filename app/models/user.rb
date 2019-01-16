3eef85491e8183dd1651ee4a0d4fabeec5ad629d
simp338 <45528081+simp338@users.noreply.github.com>
2019-01-16 22:53:55 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,10 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
+  
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids+[self.id])
+    #投稿のuser_idがフォローしている人のidと、自分のidであるものを抽出するメソッド => toppagesコントローラで使う
+  end
 
 end
